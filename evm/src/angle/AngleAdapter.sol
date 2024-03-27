045fa1cc0d045a7a1fbd8a89643c21759c316ecd
Ale personal <bergamaschi400@gmail.com>
2024-03-27 14:59:07 +0100

    improve: Change ITransmuter from abstract contract to interface

--- evm/src/angle/AngleAdapter.sol
+++ evm/src/angle/AngleAdapter.sol
@@ -464,82 +464,71 @@ library LibManager {
     }
 }
 
-abstract contract ITransmuter {
-    function implementation() external view returns (address) {}
+interface ITransmuter {
+    function implementation() external view returns (address);
 
-    function setDummyImplementation(address _implementation) external {}
+    function setDummyImplementation(address _implementation) external;
 
     function facetAddress(bytes4 _functionSelector)
         external
         view
-        returns (address facetAddress_)
-    {}
+        returns (address facetAddress_);
 
     function facetAddresses()
         external
         view
-        returns (address[] memory facetAddresses_)
-    {}
+        returns (address[] memory facetAddresses_);
 
     function facetFunctionSelectors(address _facet)
         external
         view
-        returns (bytes4[] memory _facetFunctionSelectors)
-    {}
+        returns (bytes4[] memory _facetFunctionSelectors);
 
-    function accessControlManager() external view returns (address) {}
+    function accessControlManager() external view returns (address);
 
-    function agToken() external view returns (address) {}
+    function agToken() external view returns (address);
 
     function getCollateralBurnFees(address collateral)
         external
         view
-        returns (uint64[] memory xFeeBurn, int64[] memory yFeeBurn)
-    {}
+        returns (uint64[] memory xFeeBurn, int64[] memory yFeeBurn);
 
     function getCollateralDecimals(address collateral)
         external
         view
-        returns (uint8)
-    {}
+        returns (uint8);
 
     function getCollateralInfo(address collateral)
         external
         view
-        returns (Collateral memory)
-    {}
+        returns (Collateral memory);
 
-    function getCollateralList() external view returns (address[] memory) {}
+    function getCollateralList() external view returns (address[] memory);
 
     function getCollateralMintFees(address collateral)
         external
         view
-        returns (uint64[] memory xFeeMint, int64[] memory yFeeMint)
-    {}
+        returns (uint64[] memory xFeeMint, int64[] memory yFeeMint);
 
     function getCollateralRatio()
         external
         view
-        returns (uint64 collatRatio, uint256 stablecoinsIssued)
-    {}
+        returns (uint64 collatRatio, uint256 stablecoinsIssued);
 
     function getCollateralWhitelistData(address collateral)
         external
         view
-        returns (bytes memory)
-    {}
+        returns (bytes memory);
 
     function getIssuedByCollateral(address collateral)
         external
         view
-        returns (uint256 stablecoinsFromCollateral, uint256 stablecoinsIssued)
-    {}
+        returns (uint256 stablecoinsFromCollateral, uint256 stablecoinsIssued);
 
     function getManagerData(address collateral)
         external
         view
-        returns (bool, address[] memory, bytes memory)
-    {}
+        returns (bool, address[] memory, bytes memory);
 
     function getOracle(address collateral)
         external
@@ -549,8 +538,7 @@ abstract contract ITransmuter {
             uint8 targetType,
             bytes memory oracleData,
             bytes memory targetData
-        )
-    {}
+        );
 
     function getOracleValues(address collateral)
         external
@@ -561,8 +549,7 @@ abstract contract ITransmuter {
             uint256 ratio,
             uint256 minRatio,
             uint256 redemption
-        )
-    {}
+        );
 
     function getRedemptionFees()
         external
@@ -570,108 +557,96 @@ abstract contract ITransmuter {
         returns (
             uint64[] memory xRedemptionCurve,
             int64[] memory yRedemptionCurve
-        )
-    {}
+        );
 
-    function getTotalIssued() external view returns (uint256) {}
+    function getTotalIssued() external view returns (uint256);
 
     function isPaused(address collateral, uint8 action)
         external
         view
-        returns (bool)
-    {}
+        returns (bool);
 
-    function isTrusted(address sender) external view returns (bool) {}
+    function isTrusted(address sender) external view returns (bool);
 
-    function isTrustedSeller(address sender) external view returns (bool) {}
+    function isTrustedSeller(address sender) external view returns (bool);
 
-    function isValidSelector(bytes4 selector) external view returns (bool) {}
+    function isValidSelector(bytes4 selector) external view returns (bool);
 
     function isWhitelistedCollateral(address collateral)
         external
         view
-        returns (bool)
-    {}
+        returns (bool);
 
     function isWhitelistedForCollateral(address collateral, address sender)
         external
-        returns (bool)
-    {}
+        returns (bool);
 
     function isWhitelistedForType(uint8 whitelistType, address sender)
         external
         view
-        returns (bool)
-    {}
+        returns (bool);
 
     function sellRewards(uint256 minAmountOut, bytes memory payload)
         external
-        returns (uint256 amountOut)
-    {}
+        returns (uint256 amountOut);
 
-    function addCollateral(address collateral) external {}
+    function addCollateral(address collateral) external;
 
     function adjustStablecoins(
         address collateral,
         uint128 amount,
         bool increase
-    ) external {}
+    ) external;
 
     function changeAllowance(address token, address spender, uint256 amount)
-        external
-    {}
+        external;
 
     function recoverERC20(
         address collateral,
         address token,
         address to,
         uint256 amount
-    ) external {}
+    ) external;
 
-    function revokeCollateral(address collateral) external {}
+    function revokeCollateral(address collateral) external;
 
     function setAccessControlManager(address _newAccessControlManager)
-        external
-    {}
+        external;
 
     function setOracle(address collateral, bytes memory oracleConfig)
-        external
-    {}
+        external;
 
     function setWhitelistStatus(
         address collateral,
         uint8 whitelistStatus,
         bytes memory whitelistData
-    ) external {}
+    ) external;
 
-    function toggleTrusted(address sender, uint8 t) external {}
+    function toggleTrusted(address sender, uint8 t) external;
 
     function setFees(
         address collateral,
         uint64[] memory xFee,
         int64[] memory yFee,
         bool mint
-    ) external {}
+    ) external;
 
     function setRedemptionCurveParams(uint64[] memory xFee, int64[] memory yFee)
-        external
-    {}
+        external;
 
-    function togglePause(address collateral, uint8 pausedType) external {}
+    function togglePause(address collateral, uint8 pausedType) external;
 
-    function toggleWhitelist(uint8 whitelistType, address who) external {}
+    function toggleWhitelist(uint8 whitelistType, address who) external;
 
     function quoteIn(uint256 amountIn, address tokenIn, address tokenOut)
         external
         view
-        returns (uint256 amountOut)
-    {}
+        returns (uint256 amountOut);
 
     function quoteOut(uint256 amountOut, address tokenIn, address tokenOut)
         external
         view
-        returns (uint256 amountIn)
-    {}
+        returns (uint256 amountIn);
 
     function swapExactInput(
         uint256 amountIn,
@@ -680,7 +655,7 @@ abstract contract ITransmuter {
         address tokenOut,
         address to,
         uint256 deadline
-    ) external returns (uint256 amountOut) {}
+    ) external returns (uint256 amountOut);
 
     function swapExactInputWithPermit(
         uint256 amountIn,
@@ -689,7 +664,7 @@ abstract contract ITransmuter {
         address to,
         uint256 deadline,
         bytes memory permitData
-    ) external returns (uint256 amountOut) {}
+    ) external returns (uint256 amountOut);
 
     function swapExactOutput(
         uint256 amountOut,
@@ -698,7 +673,7 @@ abstract contract ITransmuter {
         address tokenOut,
         address to,
         uint256 deadline
-    ) external returns (uint256 amountIn) {}
+    ) external returns (uint256 amountIn);
 
     function swapExactOutputWithPermit(
         uint256 amountOut,
@@ -707,20 +682,19 @@ abstract contract ITransmuter {
         address to,
         uint256 deadline,
         bytes memory permitData
-    ) external returns (uint256 amountIn) {}
+    ) external returns (uint256 amountIn);
 
     function quoteRedemptionCurve(uint256 amount)
         external
         view
-        returns (address[] memory tokens, uint256[] memory amounts)
-    {}
+        returns (address[] memory tokens, uint256[] memory amounts);
 
     function redeem(
         uint256 amount,
         address receiver,
         uint256 deadline,
         uint256[] memory minAmountOuts
-    ) external returns (address[] memory tokens, uint256[] memory amounts) {}
+    ) external returns (address[] memory tokens, uint256[] memory amounts);
 
     function redeemWithForfeit(
         uint256 amount,
@@ -728,10 +702,9 @@ abstract contract ITransmuter {
         uint256 deadline,
         uint256[] memory minAmountOuts,
         address[] memory forfeitTokens
-    ) external returns (address[] memory tokens, uint256[] memory amounts) {}
+    ) external returns (address[] memory tokens, uint256[] memory amounts);
 
     function updateNormalizer(uint256 amount, bool increase)
         external
-        returns (uint256)
-    {}
+        returns (uint256);
 }
