f1796d1c67a9bd206a23cc2b47b308987d7baf45
0xMochan <mochanbot@gmail.com>
2024-06-03 09:41:14 -0400

    fix: clean up `abis`

--- substreams/ethereum-curve/src/abi/mod.rs
+++ substreams/ethereum-curve/src/abi/mod.rs
@@ -1,17 +1,10 @@
 pub mod crypto_pool_factory;
 pub mod stableswap_factory;
 pub mod susd;
-pub mod pool_tricrypto;
-pub mod pool_crypto_swap_ng;
-pub mod pool_tricrypto2;
 pub mod crypto_swap_ng_factory;
-pub mod test;
 pub mod meta_registry;
-pub mod pool_steth;
-pub mod pool;
 pub mod tricrypto_factory;
 pub mod main_registry;
-pub mod pool_3pool;
 pub mod erc20;
 pub mod meta_pool_factory;
 pub mod crypto_swap_registry;
