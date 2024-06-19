959a08026a18ed14dce380f4aec0da43bc5ac22f
Florian Pellissier <111426680+flopell@users.noreply.github.com>
2024-06-19 10:00:14 +0200

    chore: remove unused abis

--- substreams/ethereum-curve/src/abi/mod.rs
+++ substreams/ethereum-curve/src/abi/mod.rs
@@ -1,11 +1,9 @@
 #![allow(clippy::all)]
 pub mod crypto_pool_factory;
 pub mod stableswap_factory;
-pub mod susd;
 pub mod crypto_swap_ng_factory;
 pub mod meta_registry;
 pub mod tricrypto_factory;
 pub mod main_registry;
 pub mod erc20;
 pub mod meta_pool_factory;
-pub mod crypto_swap_registry;
