baadd0212dd04656a8ea85a928ae75e1a6edf18e
Zizou <111426680+zizou0x@users.noreply.github.com>
2024-11-01 11:13:56 +0700

    fix(balancer): Ignore `PoolBalanceManaged` for not indexed pools (#106)
    Co-authored-by: zizou <111426680+flopell@users.noreply.github.com>

--- substreams/ethereum-balancer-v2/src/modules.rs
+++ substreams/ethereum-balancer-v2/src/modules.rs
@@ -122,13 +122,18 @@ pub fn map_relative_balances(
                 abi::vault::events::PoolBalanceManaged::match_and_decode(vault_log.log)
             {
                 let component_id = format!("0x{}", hex::encode(ev.pool_id));
-                deltas.extend_from_slice(&[BalanceDelta {
-                    ord: vault_log.ordinal(),
-                    tx: Some(vault_log.receipt.transaction.into()),
-                    token: ev.token.to_vec(),
-                    delta: ev.cash_delta.to_signed_bytes_be(),
-                    component_id: component_id.as_bytes().to_vec(),
-                }]);
+                if store
+                    .get_last(format!("pool:{}", &component_id[..42]))
+                    .is_some()
+                {
+                    deltas.extend_from_slice(&[BalanceDelta {
+                        ord: vault_log.ordinal(),
+                        tx: Some(vault_log.receipt.transaction.into()),
+                        token: ev.token.to_vec(),
+                        delta: ev.cash_delta.to_signed_bytes_be(),
+                        component_id: component_id.as_bytes().to_vec(),
+                    }]);
+                }
             }
 
             deltas
